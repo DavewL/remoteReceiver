4d20675d3707d360d57d793436b8b0e938d4593e
DavewL <dave.lombardo@gmail.com>
2018-10-14 10:09:00 -0400

    Update remoteReceiver.ino
    updates

--- remoteReceiver.ino
+++ remoteReceiver.ino
@@ -1,12 +1,9 @@
-#include <RF24_config.h>
-#include <printf.h>
-#include <nRF24L01.h>
 #include <RF24.h>
 #include <SPI.h>
 
 /****************** User Config ***************************/
 /***      Set this radio as radio number 0 or 1         ***/
-bool radioNumber = 1;
+bool radioNumber = 0;
 unsigned long loopCounter = 0;
 bool flash = 0;
 
@@ -27,9 +24,9 @@ void setup() {
   radio.setPALevel(RF24_PA_LOW);
   
   // Open a writing and reading pipe on each radio, with opposite addresses
-  radio.openWritingPipe(addresses[1]);
-  radio.openReadingPipe(1,addresses[0]);
-  
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);
+    
   // Start the radio listening for data
   radio.startListening();
 
@@ -38,21 +35,22 @@ void setup() {
 void loop() {
 
   byte axes[2];
+  delay(50);
 
   if( radio.available())
-  {                                                                    // Variable for the received timestamp
-      while (radio.available())                                       // While there is data ready
+  {                                               // Variable for the received timestamp
+      while (radio.available())                   // While there is data ready
       {                                   
-        radio.read( &axes, (2*sizeof(byte)) );             // Get the payload
+        radio.read( &axes, (2*sizeof(byte)) );    // Get the payload
       }
       Serial.print("X");
       Serial.print(axes[0]);
       Serial.print("Y");
       Serial.println(axes[1]);
      
-   radio.stopListening();                                        // First, stop listening so we can talk   
-   radio.write( &axes, (2*sizeof(byte)) );                       // Send the final one back.      
-   radio.startListening();                                       // Now, resume listening so we catch the next packets.     
+      radio.stopListening();                         // First, stop listening so we can talk   
+      radio.write( &axes, (2*sizeof(byte)) );        // Send the final one back.      
+      radio.startListening();                        // Now, resume listening so we catch the next packets.     
    //Serial.print(F("Sent response "));
    //Serial.println(got_time);  
    }
